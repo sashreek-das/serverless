8849dee058c0af7dbae633f9eb2938bfd4638439
Sashreek-PC <sashreek102003@gmail.com>
2024-06-25 20:27:11 +0530

    using middlewares

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,18 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+async function authMiddleware(c: any, next:any){
+  if(c.req.header("Authorization")){
+    //do some checks
+    await next()
+  }
+  else{
+    return c.text("authorization failed")
+  }
+}
+
+app.use(authMiddleware);
+
 app.post('/', async(c) => {
   const body = await c.req.json()
   console.log(body);
